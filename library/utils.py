d11150e5a3447329b10c9d80576b004cc4b8e66e
kiki-kanri <a470666@gmail.com>
2022-09-27 18:08:13 +0800

    更新檔案

--- library/utils.py
+++ library/utils.py
@@ -1,7 +1,10 @@
-from flask import jsonify, request
+from flask import request
 from functools import wraps
 from importlib import import_module
+from kikiutils.check import isint, isstr
 from validator import validate as _validate
+
+# from .p2p import node
 
 
 # Decorators
@@ -10,18 +13,23 @@ def validate(rules: dict):
     """Validate request data."""
 
     def decorator(view_func):
+        @wraps(view_func)
         def wrapped_view(*args, **kwargs):
-            # 取決於前端發送資料格式
-            result, data, _ = _validate(request.json, rules, True)
-            # result, data, _ = _validate(request.values.to_dict(), rules, True)
+            request_data = request.values.to_dict()
+
+            for k, v in request_data.copy().items():
+                if isstr(v):
+                    request_data[k] = v.strip()
+
+            result, data, _ = _validate(request_data, rules, True)
 
             if result:
                 kwargs['data'] = data
                 return view_func(*args, **kwargs)
 
-            return response('', status = 422)
+            return '', 422
 
-        return wraps(view_func)(wrapped_view)
+        return wrapped_view
 
     return decorator
 
@@ -38,6 +46,9 @@ def import_attribute(path: str):
 def ip2int(ip: str):
     """Convert string ip to int."""
 
+    if isint(ip):
+        return ip
+
     int_ip = 0
     for i in ip.split('.'):
         int_ip = int_ip << 8 | int(i)
@@ -46,6 +57,9 @@ def ip2int(ip: str):
 
 def int2ip(int_ip: int):
     """Convert int ip to string."""
+
+    if isstr(int_ip):
+        return int_ip
 
     ip = []
 
@@ -60,25 +74,3 @@ def int2ip(int_ip: int):
 
 def get_request_ip():
     return request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
-
-
-# Response
-
-def response(message = 'success', code = 0, status = 200):
-    """Return response with code and message (json).
-
-    Defaults params :
-        message : str = 'success'
-        code : int = 0
-        status: int = 200
-
-    Example return json : {
-        "code": code,
-        "message": message
-    }
-    """
-
-    return jsonify({
-        'code': code,
-        'message': message
-    }), status
